683ac7e296ad15763a9c4ac5b1a03bc0a63ceeae
ice-wiz <img_2020012@iiitm.ac.in>
2024-02-22 23:09:05 +0530

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,29 @@
+import './App.css'
+import Header from './components/Header'
+import CreateNote from './components/CreateNote'
+import {Box} from '@mui/material'
+import Notes from './components/Notes'
+import {useState} from 'react';
+import { NoteObject } from './models/note'
+
+function App() {
+
+  const [notes,setNotes]=useState<NoteObject[]>([]);
+
+  const addNotes=(note:NoteObject)=>{
+     setNotes([note,...notes]);
+  }
+
+
+  return (
+    <>
+      <Header/>
+      <Box style={{padding:20}}>
+      <CreateNote addNotes={addNotes}/>
+      <Notes/>
+      </Box>
+    </>
+  )
+}
+
+export default App
